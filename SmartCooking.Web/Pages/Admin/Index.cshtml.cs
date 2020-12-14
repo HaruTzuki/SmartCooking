018dfd692bf0ba0e0c3628fe5d4daa628d222e95
Dimitris Vlachopoulos <dvlachopoulos@impact.gr>
2020-12-15 00:15:59 +0200

    Add Icos, create Breadcrumbs, add new titles, fix issues

--- SmartCooking.Web/Pages/Admin/Index.cshtml.cs
+++ SmartCooking.Web/Pages/Admin/Index.cshtml.cs
@@ -1,16 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using SmartCooking.Data.Repository;
 using SmartCooking.Web.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SmartCooking.Web.Pages.Admin
 {
 	public class IndexModel : AdminPageModel
 	{
-		public IActionResult OnGet()
+		private readonly IItemRepository itemRepository;
+		private readonly IItemCategoryRepository itemCategoryRepository;
+		private readonly IRecipeRepository recipeRepository;
+		private readonly IUnitRepository unitRepository;
+
+		public int ItemCount { get; set; }
+		public int ItemCategoryCount { get; set; }
+		public int RecipeCount { get; set; }
+		public int UnitCount { get; set; }
+
+		public IndexModel(IItemRepository itemRepository, IItemCategoryRepository itemCategoryRepository, IRecipeRepository recipeRepository, IUnitRepository unitRepository)
+		{
+			this.itemRepository = itemRepository;
+			this.itemCategoryRepository = itemCategoryRepository;
+			this.recipeRepository = recipeRepository;
+			this.unitRepository = unitRepository;
+		}
+
+		public async Task<IActionResult> OnGet()
 		{
 			if (!CheckPermissions())
 			{
 				return RedirectToPage(Url.Content("~/Home/Index"));
 			}
+
+			ItemCount = (await itemRepository.GetItems()).Count();
+			ItemCategoryCount = (await itemCategoryRepository.GetItemCategories()).Count();
+			RecipeCount = (await recipeRepository.GetRecipeHeaders()).Count();
+			UnitCount = (await unitRepository.GetUnits()).Count();
 
 			return Page();
 		}
